3e1e92bb7104df88c59fb4b5db3d1f2a511303b2
Weihonghao <SCIN@rescomp-16-332379.stanford.edu>
2017-06-05 11:40:32 -0700

    db

--- ECM_model.py
+++ ECM_model.py
@@ -233,6 +233,7 @@ class ECMModel(object):
         decoder_outputs_ta, decoder_final_state, _ = tf.nn.raw_rnn(decode_cell, loop_fn)
         decoder_outputs = decoder_outputs_ta.stack()
         decoder_max_steps, decoder_batch_size, decoder_dim = tf.unstack(tf.shape(decoder_outputs))
+        assert (decoder_batch_size == self.batch_size and decoder_dim == self.decoder_state_size)
         decoder_outputs_reshape = tf.reshape(decoder_outputs, (decoder_batch_size,decoder_max_steps , decoder_dim))
         return decoder_outputs_reshape
 
